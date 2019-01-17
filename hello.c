3bb1dc50c3b27f3979aa51728dad6b1223bd4f54
katherinegerot <44932613+katherinegerot@users.noreply.github.com>
2019-01-17 11:34:12 -0600

    Added print statements
    Added lines printing my name and majors

--- hello.c
+++ hello.c
@@ -12,5 +12,7 @@ int main(int argc, char **argv) {
 
   printf("Hello World!\n");
   printf("Computer Engineering\n");
+  printf("Katherine Gerot\n");
+  printf("Computer Science and Math\n");
   return 0;
 }
